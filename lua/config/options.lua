96b98ad3ed7c189bed6165ba5ce34bf652239cd7
L <lillustrer012@gmail.com>
2025-06-16 09:28:33 +0700

    toggle screenkey on nvim startup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -37,3 +37,5 @@ vim.opt.mouse = ""
 
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
+
+vim.opt.winblend = 0
--- lua/plugins/screenkey.lua
+++ lua/plugins/screenkey.lua
@@ -5,6 +5,16 @@ return {
   win_opts = {
     style = "full",
   },
+  config = function()
+    -- require("screenkey").setup({
+    --   font_size = 14,
+    -- })
+    vim.api.nvim_create_autocmd("VimEnter", {
+      group = vim.api.nvim_create_augroup("AutostartScreenkey", {}),
+      command = "Screenkey toggle",
+      desc = "Autostart Screenkey on VimEnter",
+    })
+  end,
 
   hl_groups = {
     ["screenkey.hl.key"] = { fg = "#cba6f7", bold = true },
