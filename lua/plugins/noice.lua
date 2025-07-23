f362174a494fe85c0e6737a302c1b4bdd1501dc3
L <lillustrer012@gmail.com>
2025-07-24 00:21:12 +0700

    feat: remove floating cmdline, change to default neovim cmd input

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/noice.nvim",
+  -- opts = function(_, opts)
+  --   opts.presets.lsp_doc_border = true
+  -- end,
+
+  opts = {
+    cmdline = {
+      -- enabled = false,
+      view = "cmdline",
+    },
+    presets = {
+      command_palette = false,
+    },
+    messsages = {
+      enabled = false,
+    },
+    popupmenu = {
+      enabled = true,
+    },
+  },
+}
