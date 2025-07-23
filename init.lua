e3c1bebbcb7fb1e534ab91d73308c21f6dd0811f
L <lillustrer012@gmail.com>
2025-07-24 00:18:55 +0700

    chore: remove opacity, transparency & blur in neovide

--- init.lua
+++ init.lua
@@ -2,6 +2,11 @@
 require("config.lazy")
 require("config.options")
 
+-- global lazyVim
+-- cursorline
+-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#1e1e2e" })
+-- vim.o.cursorline = false
+
 -- helpers
 -- local alpha = function()
 --   return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
@@ -10,9 +15,9 @@ require("config.options")
 -- neovide settings
 if vim.g.neovide then
   -- layout
-  vim.g.transparency = 0.8
-  vim.g.neovide_opacity = 0.4
-  vim.g.neovide_window_blurred = true
+  vim.g.transparency = 1
+  vim.g.neovide_opacity = 1
+  vim.g.neovide_window_blurred = false
   vim.g.neovide_padding_top = 7
   -- vim.g.neovide_background_color = "#0f1117" .. alpha()
 
@@ -24,5 +29,24 @@ if vim.g.neovide then
   vim.o.linespace = 2
 
   -- cursor
-  vim.g.neovide_cursor_hack = true
+  -- vim.g.neovide_cursor_hack = true
+  -- vim.g.neovide_cursor_animation_length = 0.15
+  -- vim.g.neovide_cursor_short_animation_length = 0.04
+  -- vim.g.neovide_cursor_trail_size = 1.0
+  vim.g.neovide_cursor_vfx_particle_lifetime = 2
+  vim.g.neovide_cursor_vfx_mode = "pixiedust" -- @param: railgun, torpedo, pixiedust
+  vim.g.neovide_cursor_vfx_opacity = 200
+  vim.g.neovide_cursor_vfx_particle_density = 2
+  vim.g.neovide_cursor_vfx_particle_speed = 10
+
+  -- others
+  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e", fg = "#cdd6f4", blend = 0 })
+  -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#1e1e2e", fg = "#cdd6f4", blend = 0 })
+
+  -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#1e1e2e" })
+  -- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "#1e1e2e", fg = "#89b4fa" })
+
+  -- vim.o.termguicolors = true
+  -- vim.o.cursorline = false
+  vim.api.nvim_set_hl(0, "CursorLine", { bg = "#010100" })
 end
