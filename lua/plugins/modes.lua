15f6d58fef593363127d6f61da10672c16ee4a8c
L <lillustrer012@gmail.com>
2025-07-23 18:35:06 +0700

    chore: modes.nvim with more colors

--- lua/plugins/modes.lua
+++ lua/plugins/modes.lua
@@ -2,6 +2,53 @@ return {
   "mvllow/modes.nvim",
   tag = "v0.2.1",
   config = function()
-    require("modes").setup()
+    local default_colors = {
+      bg = "", -- Optional bg param, defaults to Normal hl group
+      copy = "#f5c359",
+      delete = "#c75c6a",
+      change = "#c75c6a", -- Optional param, defaults to delete
+      format = "#c79585",
+      insert = "#78ccc5",
+      replace = "#245361",
+      select = "#9745be", -- Optional param, defaults to visual
+      visual = "#9745be",
+    }
+
+    local neovide_colors = {
+      bg = "", -- keep default bg
+      copy = "#fff04d", -- bright neon yellow
+      delete = "#ff4d4d", -- saturated bright red
+      change = "#ff4d4d", -- same as delete
+      format = "#ff944d", -- vivid orange
+      insert = "#33ff99", -- bright mint green
+      replace = "#33cfff", -- electric sky blue
+      select = "#e600ff", -- vibrant violet
+      visual = "#e600ff", -- same as select
+    }
+
+    require("modes").setup({
+      colors = vim.g.neovide and neovide_colors or default_colors,
+
+      -- Set opacity for cursorline and number background
+      line_opacity = 0.15,
+
+      -- Enable cursor highlights
+      set_cursor = true,
+
+      -- Enable cursorline initially, and disable cursorline for inactive windows
+      -- or ignored filetypes
+      set_cursorline = true,
+
+      -- Enable line number highlights to match cursorline
+      set_number = true,
+
+      -- Enable sign column highlights to match cursorline
+      set_signcolumn = true,
+
+      -- Disable modes highlights for specified filetypes
+      -- or enable with prefix "!" if otherwise disabled (please PR common patterns)
+      -- Can also be a function fun():boolean that disables modes highlights when true
+      ignore = { "NvimTree", "TelescopePrompt", "!minifiles" },
+    })
   end,
 }
