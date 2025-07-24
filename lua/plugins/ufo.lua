10013d5718d26df5e3ae9ff3aa728d3409317cd6
L <lillustrer012@gmail.com>
2025-07-24 14:40:32 +0700

    feat: add folding with ufo

---
+++ lua/plugins/ufo.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async", -- Required by ufo
+    },
+
+    event = "BufReadPost",
+    opts = {
+      provider_selector = function(_, _, _)
+        return { "treesitter", "indent" }
+      end,
+    },
+    config = function(_, opts)
+      require("ufo").setup(opts)
+      -- Recommended keymaps
+      vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+      vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+    end,
+  },
+}
