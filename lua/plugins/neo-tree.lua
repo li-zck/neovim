8dc64c9349346a87ffca209c060b300146def906
L <lillustrer012@gmail.com>
2025-07-28 22:55:42 +0700

    feat: replace snacks explorer with neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,5 +4,23 @@ return {
     window = {
       width = 30,
     },
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          -- Change type
+          added = "✚", -- NOTE: you can set any of these to an empty string to not show them
+          deleted = "✖",
+          modified = "",
+          renamed = "",
+          -- Status type
+          untracked = "",
+          ignored = "",
+          unstaged = "",
+          staged = "",
+          conflict = "",
+        },
+        align = "right",
+      },
+    },
   },
 }
