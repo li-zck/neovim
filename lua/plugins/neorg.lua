7b82206bb0b373196f06a8069ec4290684e5c709
L <lillustrer012@gmail.com>
2025-07-28 13:24:53 +0700

    feat: add neorg plugins

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,15 @@
+return {
+  lazy = false,
+  version = "*",
+  "nvim-neorg/neorg",
+  ft = "norg",
+  cmd = "Neorg",
+  priority = 30,
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {},
+      },
+    })
+  end,
+}
