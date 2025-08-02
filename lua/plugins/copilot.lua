422aded5c4e02d1a4d4795f9149bc3154fb59862
L <lillustrer012@gmail.com>
2025-08-02 19:31:38 +0700

    feat: remove copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,22 +1,22 @@
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    panel = {
-      enabled = false,
-    },
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      keymap = {
-        accept = "<Tab>",
-        accept_word = "<C-l>",
-        next = "<M-]>",
-        prev = "<M-[>",
-        dismiss = "<M-\\>",
-      },
-    },
-    filetypes = {
-      ["*"] = true,
-    },
-  },
+  -- "zbirenbaum/copilot.lua",
+  -- opts = {
+  --   panel = {
+  --     enabled = false,
+  --   },
+  --   suggestion = {
+  --     enabled = true,
+  --     auto_trigger = true,
+  --     keymap = {
+  --       accept = "<Tab>",
+  --       accept_word = "<C-l>",
+  --       next = "<M-]>",
+  --       prev = "<M-[>",
+  --       dismiss = "<M-\\>",
+  --     },
+  --   },
+  --   filetypes = {
+  --     ["*"] = true,
+  --   },
+  -- },
 }
