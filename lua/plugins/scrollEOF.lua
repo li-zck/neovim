a5c5b57b68c358874f12cc77192bc9307a7576b6
L <lillustrer012@gmail.com>
2025-08-02 19:32:02 +0700

    chore: adjust scrollEOF behavior

--- lua/plugins/scrollEOF.lua
+++ lua/plugins/scrollEOF.lua
@@ -1,11 +1,11 @@
 return {
-  -- "Aasim-A/scrollEOF.nvim",
-  -- event = { "CursorMoved", "WinScrolled" },
-  -- opts = {
-  --   pattern = "*",
-  --   insert_mode = false,
-  --   floating = false,
-  --   disabled_filetypes = {},
-  --   disabled_mode = { "i" },
-  -- },
+  "Aasim-A/scrollEOF.nvim",
+  event = { "CursorMoved", "WinScrolled" },
+  opts = {
+    pattern = "*",
+    insert_mode = false,
+    floating = false,
+    disabled_filetypes = {},
+    disabled_mode = { "i" },
+  },
 }
