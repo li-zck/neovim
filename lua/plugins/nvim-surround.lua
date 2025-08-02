dd130de55d800489dcac9c768aeb67acfb653840
L <lillustrer012@gmail.com>
2025-08-02 20:05:05 +0700

    feat: awesome nvim surround plugin

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,16 @@
+return {
+  "kylechui/nvim-surround",
+  version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      keymaps = {
+        normal = "gsa",
+        delete = "gsd",
+        change = "gsc",
+        visual = "gsa",
+        -- insert = false,
+      },
+    })
+  end,
+}
