f5232a3196dd2ca28018f57f90346c13563b3aaa
L <lillustrer012@gmail.com>
2025-08-04 15:17:25 +0700

    feat: small changes to hurl.nvim

--- lua/plugins/hurl.lua
+++ lua/plugins/hurl.lua
@@ -24,26 +24,25 @@ return {
     -- split_position = "bottom",
     -- split_size = "50%",
 
-    -- Default formatter
     formatters = {
-      json = { "jq" }, -- Make sure you have install jq in your system, e.g: brew install jq
+      json = { "jq" },
       html = {
-        "prettier", -- Make sure you have install prettier in your system, e.g: npm install -g prettier
+        "prettierd",
         "--parser",
         "html",
       },
       xml = {
-        "tidy", -- Make sure you have installed tidy in your system, e.g: brew install tidy-html5
+        "tidy",
         "-xml",
         "-i",
         "-q",
       },
     },
-    -- Default mappings for the response popup or split views
+
     mappings = {
-      close = "q", -- Close the response popup or split view
-      next_panel = "<C-n>", -- Move to the next response popup window
-      prev_panel = "<C-p>", -- Move to the previous response popup window
+      close = "q",
+      next_panel = "<C-n>",
+      prev_panel = "<C-p>",
     },
   },
   keys = {
