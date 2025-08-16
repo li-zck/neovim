041ebb4d6d4883b7b38d00f87622732b66b485d3
L <lillustrer012@gmail.com>
2025-08-17 01:14:38 +0700

    feat: add gruvbox colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -19,61 +19,68 @@ return {
   -- },
 
   -- ROSE-PINE --
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   lazy = false,
+  --   config = function()
+  --     require("rose-pine").setup({
+  --       variant = "moon",
+  --       dark_variant = "moon",
+  --       styles = {
+  --         transparency = true,
+  --         bold = true,
+  --         italic = true,
+  --       },
+  --       enable = {
+  --         terminal = true,
+  --         legacy_highlights = true,
+  --         migrations = true,
+  --       },
+  --       groups = {
+  --         border = "muted",
+  --         link = "iris",
+  --         panel = "surface",
+  --         error = "love",
+  --         hint = "iris",
+  --         info = "foam",
+  --         note = "pine",
+  --         todo = "rose",
+  --         warn = "gold",
+  --         git_add = "foam",
+  --         git_change = "rose",
+  --         git_delete = "love",
+  --         git_dirty = "rose",
+  --         git_ignore = "muted",
+  --         git_merge = "iris",
+  --         git_rename = "pine",
+  --         git_stage = "iris",
+  --         git_text = "rose",
+  --         git_untracked = "subtle",
+  --         h1 = "iris",
+  --         h2 = "foam",
+  --         h3 = "rose",
+  --         h4 = "gold",
+  --         h5 = "pine",
+  --         h6 = "foam",
+  --       },
+  --       highlight_groups = {
+  --         Normal = { bg = nil },
+  --         NormalFloat = { bg = nil },
+  --         SignColumn = { bg = nil },
+  --         LineNr = { bg = nil },
+  --         EndOfBuffer = { bg = nil },
+  --       },
+  --     })
+  --     vim.cmd("colorscheme rose-pine-moon")
+  --   end,
+  -- },
+
+  -- GRUV BOX --
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    lazy = false,
-    config = function()
-      require("rose-pine").setup({
-        variant = "moon",
-        dark_variant = "moon",
-        styles = {
-          transparency = true,
-          bold = true,
-          italic = true,
-        },
-        enable = {
-          terminal = true,
-          legacy_highlights = true,
-          migrations = true,
-        },
-        groups = {
-          border = "muted",
-          link = "iris",
-          panel = "surface",
-          error = "love",
-          hint = "iris",
-          info = "foam",
-          note = "pine",
-          todo = "rose",
-          warn = "gold",
-          git_add = "foam",
-          git_change = "rose",
-          git_delete = "love",
-          git_dirty = "rose",
-          git_ignore = "muted",
-          git_merge = "iris",
-          git_rename = "pine",
-          git_stage = "iris",
-          git_text = "rose",
-          git_untracked = "subtle",
-          h1 = "iris",
-          h2 = "foam",
-          h3 = "rose",
-          h4 = "gold",
-          h5 = "pine",
-          h6 = "foam",
-        },
-        highlight_groups = {
-          Normal = { bg = nil },
-          NormalFloat = { bg = nil },
-          SignColumn = { bg = nil },
-          LineNr = { bg = nil },
-          EndOfBuffer = { bg = nil },
-        },
-      })
-      vim.cmd("colorscheme rose-pine-moon")
-    end,
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
   },
 
   -- ELDRITCH --
