e65ff8cf7740f480c939f2a2bf053979807e556c
L <lillustrer012@gmail.com>
2025-08-21 12:29:39 +0700

    fix: remove keymaps

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -1,22 +1,22 @@
 return {
   -- Incremental rename
-  {
-    "smjonas/inc-rename.nvim",
-    cmd = "IncRename",
-    keys = {
-      {
-        "<leader>rn",
-        function()
-          return ":IncRename " .. vim.fn.expand("<cword>")
-        end,
-        desc = "Incremental Rename",
-        mode = { "n", "v" },
-        noremap = true,
-        expr = true,
-      },
-    },
-    config = true,
-  },
+  -- {
+  --   "smjonas/inc-rename.nvim",
+  --   cmd = "IncRename",
+  --   keys = {
+  --     {
+  --       "<leader>rn",
+  --       function()
+  --         return ":IncRename " .. vim.fn.expand("<cword>")
+  --       end,
+  --       desc = "Incremental Rename",
+  --       mode = { "n", "v" },
+  --       noremap = true,
+  --       expr = true,
+  --     },
+  --   },
+  --   config = true,
+  -- },
 
   -- Refactoring tool
   -- {
