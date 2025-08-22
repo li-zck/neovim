0f5bf8fe45557ac9e475070825402b9344ff78e8
L <lillustrer012@gmail.com>
2025-08-22 19:39:12 +0700

    feat: markview autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -48,3 +48,12 @@ vim.filetype.add({
     mdx = "mdx",
   },
 })
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MarkviewAttach",
+  callback = function(event)
+    local data = event.data
+
+    vim.print(data)
+  end,
+})
