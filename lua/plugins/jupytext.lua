968e9c61bb2f4fa448f36f32195707bc84db9861
L <lillustrer012@gmail.com>
2025-08-22 19:41:06 +0700

    feat: update jupytext config

--- lua/plugins/jupytext.lua
+++ lua/plugins/jupytext.lua
@@ -3,10 +3,14 @@ return {
   lazy = false,
   config = function()
     require("jupytext").setup({
-      style = "quarto",
-      output_extension = "qmd",
-      extension = "qmd",
-      force_ft = "quarto",
+      -- style = "quarto",
+      -- output_extension = "qmd",
+      -- extension = "qmd",
+      -- force_ft = "quarto",
+      style = "markdown",
+      output_extension = "md",
+      extension = "md",
+      force_ft = "markdown",
     })
   end,
 }
