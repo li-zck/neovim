b7357c1fc3ce78482dd0eb1bc546128db52086f7
L <lillustrer012@gmail.com>
2025-08-22 19:39:38 +0700

    feat: load catppuccin theme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,16 +16,17 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
 
     { import = "plugins" },
 
     {
       "LazyVim/LazyVim",
       opts = {
-        colorscheme = "gruvbox",
+        colorscheme = "catppuccin",
+        ui = {
+          backdrop = false,
+        },
       },
     },
   },
@@ -38,8 +39,8 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  -- install = { colorscheme = { "tokyonight", "habamax" } },
-  install = { colorscheme = { "catppuccin" } },
+
+  -- install = { colorscheme = { "catppuccin" } },
 
   checker = {
     enabled = true, -- check for plugin updates periodically
