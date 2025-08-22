c92f059babffcbc944d45956d57d5995b91ff6ec
L <lillustrer012@gmail.com>
2025-08-22 19:41:45 +0700

    feat: udpate packageinfo keymaps

--- lua/plugins/packageinfo.lua
+++ lua/plugins/packageinfo.lua
@@ -5,8 +5,9 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    -- local colors = require("catppuccin.palettes").get_palette("mocha")
-    local colors = require("gruvbox")
+    local colors = require("catppuccin.palettes").get_palette("mocha")
+    local keymap = vim.keymap.set
+    local opts = { noremap = true, silent = true }
 
     require("package-info").setup({
       package_manager = "pnpm",
@@ -21,5 +22,26 @@ return {
 
       timeout = 5000,
     })
+
+    -- keybinds
+    keymap("n", "<leader>cpt", function()
+      require("package-info").toggle()
+    end, vim.tbl_extend("force", opts, { desc = "Toggle package-info" }))
+
+    keymap("n", "<leader>cpd", function()
+      require("package-info").delete()
+    end, vim.tbl_extend("force", opts, { desc = "Delete package" }))
+
+    keymap("n", "<leader>cpi", function()
+      require("package-info").install()
+    end, vim.tbl_extend("force", opts, { desc = "Install package" }))
+
+    keymap("n", "<leader>cpu", function()
+      require("package-info").update()
+    end, vim.tbl_extend("force", opts, { desc = "Update package" }))
+
+    keymap("n", "<leader>cpc", function()
+      require("package-info").change_version()
+    end, vim.tbl_extend("force", opts, { desc = "Change package version" }))
   end,
 }
