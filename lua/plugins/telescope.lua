611bb5e2c9cdd6618fd921502e6e87942ab22a1d
L <lillustrer012@gmail.com>
2025-08-22 19:42:09 +0700

    chore: disable telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,18 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   config = function()
-    require("telescope").setup()
+    -- local keymap = vim.keymap.set
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          -- keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Telescope find files" }),
+        },
+      },
+      pickers = {},
+      extensions = {},
+    })
   end,
 }
