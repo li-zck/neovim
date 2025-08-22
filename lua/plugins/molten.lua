2ca5286c56f8936dc5f54f2bfc41a4806d2a2425
L <lillustrer012@gmail.com>
2025-08-22 19:41:28 +0700

    chore: use image.nvim instead of snacks.nvim

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -7,8 +7,8 @@ return {
 
   init = function()
     -- vim.g.molten_output_win_max_height = 12
-    vim.g.molten_image_provider = "snacks.nvim"
-    vim.g.molten_auto_open_output = true
+    vim.g.molten_image_provider = "image.nvim"
+    vim.g.molten_auto_open_output = false
   end,
 
   config = function()
