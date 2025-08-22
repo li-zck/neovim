92ad361e4adb5516a817778443d8c75757d13544
L <lillustrer012@gmail.com>
2025-08-22 19:40:46 +0700

    feat: update dap keymaps

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -10,6 +10,7 @@ return {
     config = function()
       local dap = require("dap")
       local dapui = require("dapui")
+      local keymap = vim.keymap.set
 
       dapui.setup()
 
@@ -25,6 +26,32 @@ return {
       dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
       end
+
+      -- keymaps
+      keymap("n", "<leader>di", "<cmd>lua require('dap').step_into()<CR>", { desc = "Debugger step into" })
+      keymap("n", "<leader>dj", "<cmd>lua require('dap').step_over()<CR>", { desc = "Debugger step over" })
+      keymap("n", "<leader>dk", "<cmd>lua require('dap').step_out()<CR>", { desc = "Debugger step out" })
+      keymap("n", "<leader>dc", "<cmd>lua require('dap').continue()<CR>", { desc = "Debugger continue" })
+      keymap(
+        "n",
+        "<leader>db",
+        "<cmd>lua require('dap').toggle_breakpoint()<CR>",
+        { desc = "Debugger toggle breakpoint" }
+      )
+
+      -- custom: set conditional breakpoint
+      keymap(
+        "n",
+        "<leader>dd",
+        "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+        { desc = "Debugger set conditional breakpoint" }
+      )
+
+      keymap("n", "<leader>de", "<cmd>lua require('dap').terminate()<CR>", { desc = "Debugger reset" })
+      keymap("n", "<leader>dr", "<cmd>lua require('dap').run_last()<CR>", { desc = "Debugger run last" })
+
+      -- DAP PYTHON --
+      keymap("n", "<leader>dpr", "<cmd>lua require('dap-python').test_method()<CR>", { desc = "DAP Python Test" })
     end,
   },
 
