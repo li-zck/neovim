d33f6d61be2879d145356647d548c50680d378ac
L <lillustrer012@gmail.com>
2025-08-28 15:15:48 +0700

    chore: setup wilder, but nothing works

---
+++ lua/plugins/wilder.lua
@@ -0,0 +1,38 @@
+return {
+  -- "gelguy/wilder.nvim",
+  -- config = function()
+  --   local wilder = require("wilder")
+  --   wilder.setup({ modes = { ":", "/", "?" } })
+  --
+  --   local gradient = {
+  --     "#f4468f",
+  --     "#fd4a85",
+  --     "#ff507a",
+  --     "#ff566f",
+  --     "#ff5e63",
+  --     "#ff6658",
+  --     "#ff704e",
+  --     "#ff7a45",
+  --     "#ff843d",
+  --     "#ff9036",
+  --     "#f89b31",
+  --     "#efa72f",
+  --     "#e6b32e",
+  --     "#dcbe30",
+  --     "#d2c934",
+  --     "#c8d43a",
+  --     "#bfde43",
+  --     "#b6e84e",
+  --     "#aff05b",
+  --   }
+
+  -- wilder.set_option(
+  --   "renderer",
+  --   wilder.popupmenu_renderer({
+  --     highlighter = wilder.basic_highlighter(),
+  --     left = { " ", wilder.popupmenu_devicons() },
+  --     right = { " ", wilder.popupmenu_scrollbar() },
+  --   })
+  -- )
+  -- end,
+}
