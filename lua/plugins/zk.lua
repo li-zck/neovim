33fef212029cc6b19d993fb655e86c173ab36a3d
L <lillustrer012@gmail.com>
2025-08-30 16:00:53 +0700

    feat: add zk for utilizing markdown experiences

---
+++ lua/plugins/zk.lua
@@ -0,0 +1,20 @@
+return {
+  "zk-org/zk-nvim",
+  config = function()
+    require("zk").setup({
+      picker = "select",
+
+      lsp = {
+        config = {
+          name = "zk",
+          cmd = { "zk", "lsp" },
+          filetypes = { "markdown" },
+        },
+
+        auto_attach = {
+          enabled = true,
+        },
+      },
+    })
+  end,
+}
