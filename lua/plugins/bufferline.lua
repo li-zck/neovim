5033a1fdafff849914f484601db6d2512d9ee711
L <lillustrer012@gmail.com>
2025-09-04 10:13:12 +0700

    feat: update bufferline behavior to fix the issue

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,8 +3,25 @@ return {
   version = "*",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    "catppuccin",
   },
-  lazy = true,
   event = "BufReadPre",
-  opts = {},
+  init = function()
+    local ok, bufline = pcall(require, "catppuccin.groups.integrations.bufferline")
+    if ok and bufline.get_theme and not bufline.get then
+      bufline.get = bufline.get_theme
+    end
+  end,
+  opts = function(_, opts)
+    opts = opts or {}
+    local ok, bufline = pcall(require, "catppuccin.groups.integrations.bufferline")
+    if ok then
+      local getter = bufline.get or bufline.get_theme
+      if type(getter) == "function" then
+        opts.highlights = getter()
+      end
+    end
+
+    return opts
+  end,
 }
