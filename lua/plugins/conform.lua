d4ef55f5595cebd261e002a95df5e7a4a4807982
L <lillustrer012@gmail.com>
2025-09-04 10:24:06 +0700

    chore: minor update to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,15 +14,23 @@ return {
       },
       javascript = {
         "prettierd",
+        "prettier",
+        stop_after_first = true,
       },
       typescript = {
         "prettierd",
+        "prettier",
+        stop_after_first = true,
       },
       typescriptreact = {
         "prettierd",
+        "prettier",
+        stop_after_first = true,
       },
       javascriptreact = {
         "prettierd",
+        "prettier",
+        stop_after_first = true,
       },
     },
     format_on_save = {
