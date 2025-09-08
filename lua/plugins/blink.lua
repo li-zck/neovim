94164b9f13fffdb998c0c953c9d9d8dd7960de13
L <lillustrer012@gmail.com>
2025-09-08 16:05:40 +0700

    feat: adjust blink-cmp to use luasnip as snippet backend

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,10 +1,17 @@
 return {
   "saghen/blink.cmp",
   dependencies = {
+    "L3MON4D3/LuaSnip",
     "rafamadriz/friendly-snippets",
   },
-  version = "1.*",
   opts = {
+    snippets = { preset = "luasnip" },
+    sources = { default = {
+      "lsp",
+      "path",
+      "snippets",
+      "buffer",
+    } },
     completion = {
       documentation = {
         auto_show = true,
@@ -26,5 +33,31 @@ return {
         ["<Tab>"] = { "show", "accept" },
       },
     },
+
+    keymap = {
+      ["<Tab>"] = {
+        "snippet_forward",
+        "select_next",
+        "accept",
+        "fallback",
+      },
+      ["<S-Tab>"] = {
+        "snippet_backward",
+        "select_prev",
+        "fallback",
+      },
+    },
   },
+
+  config = function(_, opts)
+    require("blink.cmp").setup(opts)
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    require("luasnip.loaders.from_vscode").lazy_load({
+      paths = {
+        vim.fn.stdpath("config") .. "/snippets",
+      },
+    })
+  end,
 }
