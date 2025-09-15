24d1cc1e20abc66c2234c278a0cee099b6c276b8
L <lillustrer012@gmail.com>
2025-09-15 11:48:43 +0700

    feat: add more lsps, adjust inlay hints for gopls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -121,7 +121,6 @@ return {
         },
 
         omnisharp = {
-
           -- on_attach = function(client, bufnr)
           --   -- tell Neovim this server has no inlay hints
           --   client.server_capabilities.inlayHintProvider = false
@@ -155,6 +154,51 @@ return {
                 ForImplicitObjectCreation = true,
               },
             },
+          },
+        },
+
+        bashls = {},
+
+        dockerls = {},
+
+        gopls = {
+          settings = {
+            gofumpt = true,
+
+            codelenses = {
+              gc_details = false,
+              generate = true,
+              regenerate_cgo = true,
+              run_govulncheck = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
+            },
+
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+
+            analyses = {
+              fieldalignment = true,
+              nilness = true,
+              unusedparams = true,
+              unusedwrite = true,
+              useany = true,
+            },
+
+            usePlaceholders = true,
+            completeUnimported = true,
+            staticcheck = true,
+            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+            semanticTokens = true,
           },
         },
       },
