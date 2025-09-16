9eca1fa14678e1e601a55528c49a90d122f68e05
L <lillustrer012@gmail.com>
2025-09-16 14:23:30 +0700

    add more formatters for different filetypes with conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,34 +8,49 @@ return {
       go = {
         "gofmt",
       },
+      python = function(bufnr)
+        if require("conform").get_formatter_info("ruff_format", bufnr).available then
+          return { "ruff_format" }
+        else
+          return { "isort", "black" }
+        end
+      end,
       rust = {
         "rustfmt",
         lsp_format = "fallback",
       },
       javascript = {
         "prettierd",
-        "prettier",
         stop_after_first = true,
       },
       typescript = {
         "prettierd",
-        "prettier",
         stop_after_first = true,
       },
       typescriptreact = {
         "prettierd",
-        "prettier",
         stop_after_first = true,
       },
       javascriptreact = {
         "prettierd",
-        "prettier",
         stop_after_first = true,
       },
+
+      ["*"] = { "codespell" }, -- run formatters on all filetypes
+      ["_"] = { "trim_whitespace" }, -- run formatters on filetypes tha don't have other formatters configured.
     },
-    format_on_save = {
-      timeout_ms = 500,
+    default_format_opts = {
       lsp_format = "fallback",
     },
+    format_on_save = {
+      lsp_format = "fallback",
+      timeout_ms = 500,
+    },
+    format_after_save = {
+      lsp_format = "fallback",
+    },
+    log_level = vim.log.levels.ERROR,
+    notify_on_error = true,
+    notify_no_formatters = true,
   }),
 }
