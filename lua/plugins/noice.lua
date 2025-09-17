6ad02fc0c0a9d1e7f60276acaf495f88d254d967
L <lillustrer012@gmail.com>
2025-09-17 19:02:14 +0700

    adjust noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
+
   opts = {
     cmdline = {
       enabled = true,
@@ -23,6 +24,42 @@ return {
 
     popupmenu = {
       enabled = true,
+      -- relative = "editor",
+      -- position = {
+      --   row = 8,
+      --   col = "50%",
+      -- },
+      -- size = {
+      --   width = 60,
+      --   height = 10,
+      -- },
+      -- border = {
+      --   style = "rounded",
+      --   padding = { 0, 1 },
+      -- },
+      -- win_options = {
+      --   winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      -- },
+    },
+
+    views = {
+      cmdline_popup = {
+        border = {
+          style = "none",
+          padding = { 2, 3 },
+        },
+        filter_options = {},
+        win_options = {
+          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        },
+      },
+    },
+
+    routes = {
+      view = "notify",
+      filter = {
+        event = "msg_showmode",
+      },
     },
 
     lsp = {
