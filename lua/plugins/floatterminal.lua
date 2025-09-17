a61b81efa3751496f71066e0e4e5879d43eae350
L <lillustrer012@gmail.com>
2025-09-17 19:01:56 +0700

    refactor: refactor float terminal behavior

--- lua/plugins/floatterminal.lua
+++ lua/plugins/floatterminal.lua
@@ -6,33 +6,21 @@ return {
   config = function()
     vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 
-    local state = {
-      floating = {
-        buf = -1,
-        win = -1,
-      },
-    }
+    local state = { win = nil, buf = nil }
 
     local function create_floating_window(opts)
       opts = opts or {}
       local width = opts.width or math.floor(vim.o.columns * 0.8)
       local height = opts.height or math.floor(vim.o.lines * 0.8)
-
-      -- calculate the position to center the window
       local col = math.floor((vim.o.columns - width) / 2)
       local row = math.floor((vim.o.lines - height) / 2)
 
-      -- create a buffer
-      local buf = nil
-      if vim.api.nvim_buf_is_valid(opts.buf) then
-        buf = opts.buf
-      else
-        -- create a new buffer if the previous one is invalid
-        buf = vim.api.nvim_create_buf(false, true)
-      end
+      -- re-create buffer if valid, else create new
+      local buf = (opts.buf and vim.api.nvim_buf_is_valid(opts.buf)) and opts.buf
+        or vim.api.nvim_create_buf(false, true)
 
       -- define window configuration
-      local win_opts = {
+      local win_opts = vim.api.nvim_open_win(buf, true, {
         relative = "editor",
         width = width,
         height = height,
@@ -40,35 +28,45 @@ return {
         row = row,
         style = "minimal", -- no borders or extras UI elements
         border = "rounded",
-      }
+      })
 
       -- create the floating window
-      local win = vim.api.nvim_open_win(buf, true, win_opts)
+      if vim.bo[buf].buftype ~= "terminal" then
+        vim.cmd("terminal")
+      end
 
-      return { buf = buf, win = win }
+      local function hide_if_valid()
+        if win_opts and vim.api.nvim_buf_is_valid(win_opts) then
+          vim.api.nvim_win_hide(win_opts)
+        end
+      end
+
+      vim.keymap.set("n", "q", hide_if_valid, { buffer = buf, nowait = true, silent = true })
+      vim.keymap.set("t", "q", function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
+        hide_if_valid()
+      end, { buffer = buf, nowait = true, silent = true })
+
+      vim.bo[buf].bufhidden = "hide"
+
+      return win_opts, buf
     end
 
     local toggle_terminal = function()
-      if not vim.api.nvim_win_is_valid(state.floating.win) then
-        state.floating = create_floating_window({ buf = state.floating.buf })
-        if vim.bo[state.floating.buf].buftype ~= "terminal" then
-          vim.cmd.terminal()
-          -- vim.cmd("setlocal winhighlight=Normal:NormalFloat") -- for neovide specifically
-        end
+      if not (state.win and vim.api.nvim_win_is_valid(state.win)) then
+        state.win, state.buf = create_floating_window({ buf = state.buf })
       else
-        vim.api.nvim_win_hide(state.floating.win)
+        vim.api.nvim_win_hide(state.win)
       end
-      -- print(vim.inspect(state.floating))
     end
 
     local close_terminal = function()
-      if vim.api.nvim_win_is_valid(state.floating.win) then
-        vim.api.nvim_win_hide(state.floating.win)
+      if state.win and vim.api.nvim_win_is_valid(state.win) then
+        vim.api.nvim_win_hide(state.win)
       end
     end
 
     vim.api.nvim_create_user_command("FloatTerminal", toggle_terminal, {})
     vim.keymap.set({ "n", "t" }, "<leader>tt", toggle_terminal, { desc = "Toggle Floating Terminal" })
-    vim.keymap.set({ "n", "t" }, "q", close_terminal, { desc = "Close Floating Terminal" })
   end,
 }
