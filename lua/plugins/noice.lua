801cdb4da36e7e1dac1229f965df0b503509761a
L <lillustrer012@gmail.com>
2025-09-18 19:10:35 +0700

    change styling of cmdline with noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -43,16 +43,16 @@ return {
     },
 
     views = {
-      cmdline_popup = {
-        border = {
-          style = "none",
-          padding = { 2, 3 },
-        },
-        filter_options = {},
-        win_options = {
-          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-        },
-      },
+      -- cmdline_popup = {
+      --   border = {
+      --     style = "none",
+      --     padding = { 2, 3 },
+      --   },
+      --   filter_options = {},
+      --   win_options = {
+      --     winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+      --   },
+      -- },
     },
 
     routes = {
