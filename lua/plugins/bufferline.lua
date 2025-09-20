7ddb99cc9b05ead42bd828cf6d7f4b0d169ddf3b
L <lillustrer012@gmail.com>
2025-09-20 22:41:00 +0700

    update bufferline settings

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,23 +5,47 @@ return {
     "nvim-tree/nvim-web-devicons",
     "catppuccin",
   },
+
   event = "BufReadPre",
+
   init = function()
     local ok, bufline = pcall(require, "catppuccin.groups.integrations.bufferline")
     if ok and bufline.get_theme and not bufline.get then
       bufline.get = bufline.get_theme
     end
   end,
-  opts = function(_, opts)
-    opts = opts or {}
-    local ok, bufline = pcall(require, "catppuccin.groups.integrations.bufferline")
-    if ok then
-      local getter = bufline.get or bufline.get_theme
-      if type(getter) == "function" then
-        opts.highlights = getter()
-      end
-    end
 
-    return opts
-  end,
+  opts = {
+    options = {
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      separator_style = "slope",
+
+      offsets = {
+        {
+          separator = false,
+        },
+      },
+    },
+  },
+
+  -- opts = function(_, opts)
+  --   opts = opts or {}
+  --   local ok, catpp_bufline = pcall(require, "catppuccin.groups.integrations.bufferline")
+  --   if ok then
+  --     local getter = catpp_bufline.get or catpp_bufline.get_theme
+  --     if type(getter) == "function" then
+  --       opts.highlights = getter()
+  --     end
+  --   end
+  --
+  --   opts.options = vim.tbl_deep_extend("force", opts.options or {}, {
+  --     options = {
+  --       show_buffer_close_icons = false,
+  --       show_close_icon = false,
+  --     },
+  --   })
+  --
+  --   return opts
+  -- end,
 }
