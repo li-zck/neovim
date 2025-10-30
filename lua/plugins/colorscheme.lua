abbcabf55f3c364b7edf398bb876c78b7d835f7d
L <lillustrer012@gmail.com>
2025-10-30 21:15:23 +0700

    add everforest theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -186,4 +186,19 @@ return {
   --     end,
   --   },
   -- },
+
+  -- {
+  --   "neanias/everforest-nvim",
+  --   version = false,
+  --   lazy = false,
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   -- Optional; default configuration will be used if setup isn't called.
+  --   config = function()
+  --     require("everforest").setup({
+  --       background = "hard",
+  --       italics = false,
+  --       -- Your config here
+  --     })
+  --   end,
+  -- },
 }
